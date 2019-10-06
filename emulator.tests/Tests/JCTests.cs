e8504b6a16d4d9cc4f3b69b7f8c3d0d8c20b8f9d
Justin Unterreiner <justin.unterreiner@gmail.com>
2019-10-05 21:00:00 -0700

    Added tests for jump instructions

---
+++ emulator.tests/Tests/JCTests.cs
@@ -0,0 +1,83 @@
+using Xunit;
+
+namespace JustinCredible.SIEmulator.Tests
+{
+    public class JCTests : BaseTest
+    {
+        [Fact]
+        public void TestJC_Jumps()
+        {
+            var rom = AssembleSource($@"
+                org 00h
+                NOP         ; $0000
+                NOP         ; $0001
+                JC 000Ah    ; $0002
+                HLT         ; $0005
+                NOP         ; $0006
+                NOP         ; $0007
+                NOP         ; $0008
+                NOP         ; $0009
+                HLT         ; $000A
+            ");
+
+            var initialState = new InitialCPUState()
+            {
+                Flags = new ConditionFlags()
+                {
+                    Carry = true,
+                },
+            };
+
+            var state = Execute(rom, initialState);
+
+            Assert.Equal(0x0000, state.StackPointer);
+
+            Assert.True(state.Flags.Carry);
+            Assert.False(state.Flags.Parity);
+            Assert.False(state.Flags.Sign);
+            Assert.False(state.Flags.Zero);
+
+            Assert.Equal(4, state.Iterations);
+            Assert.Equal(7 + (4*2) + 10, state.Cycles);
+            Assert.Equal(0x000A, state.ProgramCounter);
+        }
+
+        [Fact]
+        public void TestJC_DoesNotJump()
+        {
+            var rom = AssembleSource($@"
+                org 00h
+                NOP         ; $0000
+                NOP         ; $0001
+                JC 000Ah    ; $0002
+                HLT         ; $0005
+                NOP         ; $0006
+                NOP         ; $0007
+                NOP         ; $0008
+                NOP         ; $0009
+                HLT         ; $000A
+            ");
+
+            var initialState = new InitialCPUState()
+            {
+                Flags = new ConditionFlags()
+                {
+                    Carry = false,
+                },
+            };
+
+            var state = Execute(rom, initialState);
+
+            Assert.Equal(0x0000, state.StackPointer);
+
+            Assert.False(state.Flags.Carry);
+            Assert.False(state.Flags.Parity);
+            Assert.False(state.Flags.Sign);
+            Assert.False(state.Flags.Zero);
+
+            Assert.Equal(4, state.Iterations);
+            Assert.Equal(7 + (4*2) + 10, state.Cycles);
+            Assert.Equal(0x0005, state.ProgramCounter);
+        }
+    }
+}
---
+++ emulator.tests/Tests/JNZTests.cs
@@ -0,0 +1,83 @@
+using Xunit;
+
+namespace JustinCredible.SIEmulator.Tests
+{
+    public class JNZTests : BaseTest
+    {
+        [Fact]
+        public void TestJNZ_Jumps()
+        {
+            var rom = AssembleSource($@"
+                org 00h
+                NOP         ; $0000
+                NOP         ; $0001
+                JNZ 000Ah    ; $0002
+                HLT         ; $0005
+                NOP         ; $0006
+                NOP         ; $0007
+                NOP         ; $0008
+                NOP         ; $0009
+                HLT         ; $000A
+            ");
+
+            var initialState = new InitialCPUState()
+            {
+                Flags = new ConditionFlags()
+                {
+                    Zero = false,
+                },
+            };
+
+            var state = Execute(rom, initialState);
+
+            Assert.Equal(0x0000, state.StackPointer);
+
+            Assert.False(state.Flags.Carry);
+            Assert.False(state.Flags.Parity);
+            Assert.False(state.Flags.Sign);
+            Assert.False(state.Flags.Zero);
+
+            Assert.Equal(4, state.Iterations);
+            Assert.Equal(7 + (4*2) + 10, state.Cycles);
+            Assert.Equal(0x000A, state.ProgramCounter);
+        }
+
+        [Fact]
+        public void TestJNZ_DoesNotJump()
+        {
+            var rom = AssembleSource($@"
+                org 00h
+                NOP         ; $0000
+                NOP         ; $0001
+                JNZ 000Ah    ; $0002
+                HLT         ; $0005
+                NOP         ; $0006
+                NOP         ; $0007
+                NOP         ; $0008
+                NOP         ; $0009
+                HLT         ; $000A
+            ");
+
+            var initialState = new InitialCPUState()
+            {
+                Flags = new ConditionFlags()
+                {
+                    Zero = true,
+                },
+            };
+
+            var state = Execute(rom, initialState);
+
+            Assert.Equal(0x0000, state.StackPointer);
+
+            Assert.False(state.Flags.Carry);
+            Assert.False(state.Flags.Parity);
+            Assert.False(state.Flags.Sign);
+            Assert.True(state.Flags.Zero);
+
+            Assert.Equal(4, state.Iterations);
+            Assert.Equal(7 + (4*2) + 10, state.Cycles);
+            Assert.Equal(0x0005, state.ProgramCounter);
+        }
+    }
+}
