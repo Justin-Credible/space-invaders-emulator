a706605e2f18a69e99c1f1c96d5018f715af4035
Justin Unterreiner <justin.unterreiner@gmail.com>
2020-03-10 22:06:44 -0700

    Rename sound effects and add enumeration

---
+++ emulator/SpaceInvaders/SoundEffect.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace JustinCredible.SIEmulator
+{
+    public enum SoundEffect
+    {
+        UFO,
+        UFOHit,
+        Shot,
+        InvaderKilled,
+        PlayerDied,
+        InvaderMove1,
+        InvaderMove2,
+        InvaderMove3,
+        InvaderMove4,
+    }
+}
